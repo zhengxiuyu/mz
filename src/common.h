9878a1147effe042f5a7aa08c65fbf2d6b133a1a
arno luebke <aiwl@dn157-099.naist.jp>
2015-11-25 23:30:47 +0900

    cleanup

--- src/common.h
+++ src/common.h
@@ -7,17 +7,16 @@
 #include <math.h>
 
 #ifdef DEBUG
-    #define mz_debug(...)                                                          \
+    #define mz_debug(...)                                                       \
         printf("[mz] %-10s(%-5s): ", __FILE__, __LINE__);                       \
         printf(__VA_ARGS__)
 #else
-    #define debug(...)
+    #define mz_debug(...)
 #endif
 
 #define mz_checkmem(x) if (!(x)) goto error
-
 #define mz_min(a, b) (a) < (b) ? (a) : (b)
 #define mz_max(a, b) (a) > (b) ? (a) : (b)
-#define mz_clamp(x, a, b) mz_min(b, mz_max(x, a))
+#define mz_clamp(x, a, b) mz_min((b), mz_max((x), (a)))
 
 #endif /* end of include guard: COMMON_H */
