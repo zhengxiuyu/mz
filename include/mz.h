ba640d8051fd053317bc31cc30003315b2de575a
arno luebke <aiwl@naist-wavenet124-243.naist.jp>
2015-12-02 01:00:56 +0900

    backup

--- include/mz.h
+++ include/mz.h
@@ -10,30 +10,20 @@ enum {
 };
 
 typedef struct {
-    float           min[2];
-    float           max[2];
-    float           extent[2];
+    float min[2];
+    float max[2];
+    float extent[2];
 } mz_domain;
 
-int mz_make_domain(mz_domain *domain, float xmin, float ymin,
-                   float xmax, float ymax);
-
 typedef struct {
-    float           rest_density;
-    float           gravity;
-    mz_domain       domain;
-} mz_params;
-
-typedef struct {
-    float           (*positions)[2];
-    float           (*velocities)[2];
-    float           *lambdas;               /* step sizes        */
-    float           *dpositions[2];         /* positions updates */
-    int             num_particles;
+    float (*positions)[2];
+    float (*velocities)[2];
+    float *lambdas;                                     /* step sizes        */
+    float *dpositions[2];                               /* positions updates */
+#ifdef
+    float *densities;
+    int num_particles;
 } mz_particles;
-
-int mz_init_particles(mz_particles *particles, unsigned int num_particles);
-void mz_deinit_particles(mz_particles *particles);
 
 /*
  * Acceleration structure for quickly finding particle neighbors.
@@ -42,41 +32,66 @@ void mz_deinit_particles(mz_particles *particles);
  * TODO: Using Z-ordering might increase memory coherency.
  */
 typedef struct {
-    mz_domain       domain;
-    float           dx;
-    int             num_cells_total;
-    int             num_cells[2];
-    int             *num_particles;     /* # particles for each cell. */
-    int             *start_ids;         /* reference to the first particle id
-                                           for each cell in [ids]. */
-    int             *ids;               /* particle ids for referencing particles
-                                           within a cell */
+    mz_domain domain;
+    float dx;
+    int num_cells_total;
+    int num_cells[2];
+    int *num_particles;                 /* # particles for each cell.         */
+    int *start_ids;                     /* reference to the first particle id
+                                           for each cell in [ids].            */
+    int *ids;                           /* particle ids for referencing
+                                           particles within a cell            */
 } mz_grid;
 
-#define mz_grid_index_from_coord(grid, coord)                                   \
+int mz_make_domain(
+	mz_domain *domain,
+    float xmin,
+    float ymin,
+    float xmax,
+    float ymax
+);
+
+
+#define mz_grid_index_from_coord(grid, coord)                                  \
     (coord)[0] * (grid)->num_cells[0] + (coord)[1]
 
-int mz_init_grid(mz_grid *grid, const mz_particles *particles,
-                 const mz_domain *domain, float dx);
-void mz_deinit_grid(mz_grid *grid);
+int mz_init_particles(mz_particles *particles, unsigned int num_particles);
+void mz_deinit_particles(mz_particles *particles);
+
+int mz_init_grid(
+	mz_grid *grid,
+    const mz_particles *particles,
+    const mz_domain *domain,
+    float dx
+);
 int mz_update_grid(mz_grid *grid, const mz_particles *particles);
+void mz_deinit_grid(mz_grid *grid);
 
 /*
  * Computes the grid coordinate of the cell containing [position] and stores
  * it in [coord]. Returns false if the position is outside the grid, otherwise
  * true.
  */
-bool mz_grid_coord_from_position(const mz_grid *grid, int coord[2],
-                                 const float position[2]);
+bool mz_grid_coord_from_position(
+	const mz_grid *grid,
+    int coord[2],
+    const float position[2]
+);
 
 /*
  * Computes the cell index for the cell containing [position] and stores it in
  * [index]. Returns false if the position is outside the grid, otherwise true.
  */
-bool mz_grid_index_from_position(const mz_grid *grid, int *index,
-                                 float position[2]);
+bool mz_grid_index_from_position(
+	const mz_grid *grid,
+    int *index,
+	float position[2]);
 
-int mz_enforce_incompressibility(mz_particles *particles, const mz_grid *grid,
-                                 float rest_density, float support);
+int mz_enforce_incompressibility(
+	mz_particles *particles,
+    const mz_grid *grid,
+    float rest_density,
+    float support
+);
 
 #endif /* end of include guard: PARTICLES_H */
